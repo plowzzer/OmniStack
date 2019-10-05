fa1d333201152104464874d45cff2ddf670e5de0
Pedro Pizzo <poloo.p3@gmail.com>
2019-10-04 23:41:03 -0300

    mobile with reactnative

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const socketio = require("socket.io");
+const http = require("http");
+
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 // req.query = get url query -> ?age=20 (/users?age=20) filters
 // req.params = get url params -> :id (/users/1) edit and delete
@@ -20,4 +25,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
