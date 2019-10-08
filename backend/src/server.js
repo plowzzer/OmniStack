f6673cf65554581cc81e803a2af4c105e907c4ca
Pedro Pizzo <poloo.p3@gmail.com>
2019-10-07 22:58:12 -0300

    Mobile and socketio

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,6 +20,20 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
